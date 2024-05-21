6414171463c5eee1cc8c709c47d41b4c8045bb80
AdrianNetto <adrian.netto73@gmail.com>
2024-05-21 17:13:57 -0300

    feat: login functionality

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import News from "@/components/News";
 import Sidebar from "@/components/Sidebar";
+import SessionWrapper from "@/components/SessionWrapper";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,18 +13,27 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <div className="flex justify-between max-w-6xl mx-auto">
-          <div>
-            <Sidebar />
+    <SessionWrapper>
+      <html lang="en">
+        <body className={inter.className}>
+          <div className="flex justify-between max-w-6xl mx-auto">
+            <div className="hidden sm:inline border-r h-screen p-3">
+              <Sidebar />
+            </div>
+            <div>{children}</div>
+            <div className="lg:flex lg:flex-col p-3 h-screen border-l hidden w-[24rem]">
+              <div className="sticky top-0 bg-white py-2 focus:border-blue-500">
+                <input
+                  type="text"
+                  placeholder="Search"
+                  className="bg-gray-100 border border-gray-200 rounded-3xl text-sm w-full px-4 py-2"
+                ></input>
+              </div>
+              <News />
+            </div>
           </div>
-          <div>{children}</div>
-          <div>
-            <News />
-          </div>
-        </div>
-      </body>
-    </html>
+        </body>
+      </html>
+    </SessionWrapper>
   );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+"use client";
+import { signIn, signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+import React from "react";
+import { FaXTwitter } from "react-icons/fa6";
+import { HiHome } from "react-icons/hi";
 
 export default function Sidebar() {
+  const { data: session } = useSession();
+
   return (
-    <div>Sidebar</div>
-  )
+    <div className="flex flex-col gap-4">
+      <Link href="/">
+        <FaXTwitter className="w-16 h-16 cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-200" />
+      </Link>
+      <Link
+        href="/"
+        className="flex items-center p-3 hover:bg-gray-100 rounded-full transition-all duration-200 gap-2 w-fit"
+      >
+        <HiHome className="w-7 h-7" />
+        <span className="font-bold hidden xl:inline">Home</span>
+      </Link>
+
+      {session ? (
+        <button
+          onClick={() => signOut()}
+          className="bg-blue-400 text-white rounded-full hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline"
+        >
+          Sign Out
+        </button>
+      ) : (
+        <button
+          onClick={() => signIn()}
+          className="bg-blue-400 text-white rounded-full hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline"
+        >
+          Sign In
+        </button>
+      )}
+    </div>
+  );
 }
