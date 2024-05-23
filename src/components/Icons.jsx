fbb15405b2e90f7f951023f452e1f5b7e3dd6edc
AdrianNetto <adrian.netto73@gmail.com>
2024-05-23 15:17:26 -0300

    feat: adding recoil and implementing it

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -17,11 +17,15 @@ import {
   deleteDoc,
 } from "firebase/firestore";
 import { app } from "@/firebase";
+import { modalState } from "@/atom/modalAtom";
+import { useRecoilState } from "recoil";
 
 export default function Icons({ id, uid }) {
-  const { data: session } = useSession();
   const [isLiked, setIsLiked] = useState(false);
-  const [likes, setLikes] = useState([]); // [1
+  const [likes, setLikes] = useState([]);
+  const [open, setOpen] = useRecoilState(modalState);
+
+  const { data: session } = useSession();
   const db = getFirestore(app);
   const likePost = async () => {
     if (session) {
@@ -61,13 +65,17 @@ export default function Icons({ id, uid }) {
           });
       }
     } else {
-      alert("You are not authorized to delete this post.")
+      alert("You are not authorized to delete this post.");
     }
   };
 
   return (
     <div className="flex justify-start gap-5 p-2 text-gray-500">
-      <HiOutlineChat className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-sky-500 hover:bg-sky-100" />
+      <HiOutlineChat
+        className='h-8 w-8 cursor-pointer rounded-full  transition duration-500 ease-in-out p-2 hover:text-sky-500 hover:bg-sky-100'
+        onClick={() => setOpen(!open)}
+      />
+
       <div className="flex items-center">
         {isLiked ? (
           <HiHeart
