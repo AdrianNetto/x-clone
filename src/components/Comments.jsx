ac07201b700b85bd41b872fa9198461468d814a9
AdrianNetto <adrian.netto73@gmail.com>
2024-05-24 10:14:34 -0300

    feat: adding comment section to post page

---
+++ src/components/Comments.jsx
@@ -0,0 +1,33 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { app } from "@/firebase";
+import {
+  getFirestore,
+  onSnapshot,
+  collection,
+  query,
+  orderBy,
+} from "firebase/firestore";
+import Comment from "./Comment";
+
+export default function Comments({ id }) {
+  const db = getFirestore(app);
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    onSnapshot(
+      query(collection(db, "posts", id, "comments")),
+      orderBy("timestamp", "desc"),
+      (snapshot) => {
+        setComments(snapshot.docs);
+      }
+    );
+  }, [db, id]);
+  return (
+    <div>
+      {comments.map((comment) => (
+        <Comment key={comment.id} comment={comment.data()} id={comment.id} />
+      ))}
+    </div>
+  );
+}
