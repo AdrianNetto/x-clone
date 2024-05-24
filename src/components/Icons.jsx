cc800d38a9f47b5750c2e413aca03600ef58c5e9
AdrianNetto <adrian.netto73@gmail.com>
2024-05-24 09:05:35 -0300

    feat: adding modal basic functions

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -84,8 +84,15 @@ export default function Icons({ id, uid }) {
   return (
     <div className="flex justify-start gap-5 p-2 text-gray-500">
       <HiOutlineChat
-        className='h-8 w-8 cursor-pointer rounded-full  transition duration-500 ease-in-out p-2 hover:text-sky-500 hover:bg-sky-100'
-        onClick={() => setOpen(!open)}
+        className="h-8 w-8 cursor-pointer rounded-full  transition duration-500 ease-in-out p-2 hover:text-sky-500 hover:bg-sky-100"
+        onClick={() => {
+          if (!session) {
+            signIn();
+          } else {
+            setOpen(!open);
+            setPostId(id);
+          }
+        }}
       />
 
       <div className="flex items-center">
